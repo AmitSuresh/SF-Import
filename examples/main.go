80baa71a8c4a765e8fc4c005d751cd5f023adfcf
Amit Suresh <amit_suresh@outlook.com>
2023-08-28 20:16:36 +0530

    minor update to initialize conn

---
+++ examples/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	sfimport "github.com/AmitSuresh/SF-Import"
+)
+
+var (
+	username      string
+	password      string
+	securityToken string
+	instanceURL   string
+)
+
+func init() {
+	flag.StringVar(&username, "u", "", "username")
+	flag.StringVar(&password, "p", "", "password")
+	flag.StringVar(&securityToken, "t", "", "security token")
+	flag.StringVar(&instanceURL, "i", "", "instance URL")
+	flag.Parse()
+}
+
+func main() {
+	if username == "" || password == "" || securityToken == "" || instanceURL == "" {
+		fmt.Println("Value missing.")
+		return
+	}
+
+	sesh, err := sfimport.InitiateConnection(username, password, securityToken, instanceURL)
+	if err != nil {
+		log.Println("Error initializing connection.")
+		return
+	}
+
+	fmt.Println("App is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
